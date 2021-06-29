0acd585f10d826d7e1d21e54d07f44b286e150c3
TheNotebook0326 <1540479461@qq.com>
2021-06-29 05:02:22 -0700

    First Commit

---
+++ test002/test002/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test002
+//
+//  Created by 李玥熹 on 2021/6/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
